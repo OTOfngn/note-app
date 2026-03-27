1088b80d95d09623bd076852fbdad10c459bd010
OTOfngn <c98423795274152963@outlook.com>
2026-03-27 10:19:31 +0200

    debugging7

--- backend/db.js
+++ backend/db.js
@@ -2,6 +2,12 @@ const postgres = require('postgres');
 require('dotenv').config();
 
 const connectionString = process.env.DATABASE_URL;
+if (!connectionString) {
+    console.error("DATABASE_URL is not set in environment variables!");
+} else {
+    const host = connectionString.split('@')[1]?.split(':')[0] || "unknown";
+    console.log(`Database connection initialized for host: ${host}`);
+}
 const sql = postgres(connectionString, {
     ssl: 'require'
 });
