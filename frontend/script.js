be7a3f619430b3ff310aa05c31a7796a95b8b350
OTOfngn <c98423795274152963@outlook.com>
2026-03-27 06:47:57 +0200

    debugging

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000";
+const API_URL = ""; // Use relative path so it works both locally and on Render
 
 const noteInput = document.getElementById("note-input");
 const addBtn = document.getElementById("add-btn");
